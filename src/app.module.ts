94e1e087147320f6535311854e1ef7a01dd000a6
Alberto Benatti <albi.benni8@gmail.com>
2023-07-21 19:29:36 +0200

    init library module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
+import { LibraryModule } from './library/library/library.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), CommonModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CommonModule,
+    UserModule,
+    LibraryModule,
+  ],
 })
 export class AppModule {}
