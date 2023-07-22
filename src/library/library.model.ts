bd554e5b3b0ddeea42f20f5cb61c008ec2c5631e
Alberto Benatti <albi.benni8@gmail.com>
2023-07-22 09:51:55 +0200

    created library model

---
+++ src/library/library.model.ts
@@ -0,0 +1,38 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, ObjectId, Schema as MongooseSchema } from 'mongoose';
+import { User } from '../user/user.model';
+
+export type LibraryDocument = Library & Document;
+export type LibraryRef = Library | MongooseSchema.Types.ObjectId;
+
+export interface LibraryCreationRequest {
+  address: string;
+  managers: User[];
+  books: Book[];
+}
+
+export interface LibraryUpdateRequest {
+  manager: User[];
+  books: Book[];
+}
+
+type Book = {
+  title: string;
+  topic: string;
+};
+
+@Schema({ autoIndex: true })
+export class Library {
+  _id: ObjectId;
+
+  @Prop({ index: true, required: true, unique: true })
+  address: string;
+
+  @Prop({ type: [User], index: true })
+  managers: User[];
+
+  @Prop({ required: true })
+  books: Book[];
+}
+
+export const LibrarySchema = SchemaFactory.createForClass(Library);
