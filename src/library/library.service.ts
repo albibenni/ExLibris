d0b94eda87af36e07861c90e67a9e0d38d42e594
Alberto Benatti <albi.benni8@gmail.com>
2023-07-23 09:19:17 +0200

    added finds

--- src/library/library.service.ts
+++ src/library/library.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { LibraryRepository } from './library.repository';
 
 @Injectable()
-export class LibraryService {}
+export class LibraryService {
+  constructor(private readonly repo: LibraryRepository) {}
+  findByAddress(address: string) {
+    return this.repo.findByAddress(address);
+  }
+
+  findLibraries() {
+    return this.repo.findAll();
+  }
+}
