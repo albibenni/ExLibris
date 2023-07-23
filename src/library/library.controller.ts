9d9999e878fec30b8fb03e53e694a7b1c6ef27b1
Alberto Benatti <albi.benni8@gmail.com>
2023-07-23 12:40:52 +0200

    create library

--- src/library/library.controller.ts
+++ src/library/library.controller.ts
@@ -1,4 +1,65 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Logger,
+  NotFoundException,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { Roles, RolesGuard } from '../auth/guard/role-guard';
+import { ROLES } from '../auth/roles';
+import { LibraryService } from './library.service';
+import { dtoFromLibrary, LibraryDto } from './dto/library.dto';
+import Principal from '../auth/model/principal.model';
+import { Library, LibraryCreationRequest } from './library.model';
 
 @Controller('library')
-export class LibraryController {}
+@UseGuards(RolesGuard)
+export class LibraryController {
+  private readonly logger = new Logger(LibraryController.name);
+
+  constructor(private readonly libraryService: LibraryService) {}
+
+  @Get('libraries')
+  @Roles(ROLES.ADMIN, ROLES.MANAGER)
+  async getLibraries(): Promise<LibraryDto[]> {
+    return this.libraryService
+      .findLibraries()
+      .then((users) => users.map(dtoFromLibrary));
+  }
+
+  @Get(':address')
+  @Roles(ROLES.ADMIN, ROLES.MANAGER)
+  async getLibraryByAddress(
+    @Param('address') address: string,
+  ): Promise<LibraryDto> {
+    return this.libraryService
+      .findByAddress(address)
+      .then((lib) =>
+        lib
+          .map(dtoFromLibrary)
+          .orElseThrow(
+            () =>
+              new NotFoundException(
+                `Library with address ${address} not found`,
+              ),
+          ),
+      );
+  }
+
+  @Post('')
+  @Roles(ROLES.ADMIN, ROLES.MANAGER)
+  async createLibrary(
+    @Req() request: Request,
+    @Body() requestBody: LibraryCreationRequest,
+  ): Promise<LibraryDto> {
+    this.logger.log(`Create library with address: ${requestBody.address}`);
+    // const principal = <Principal>request.user;
+    return this.libraryService
+      .create(requestBody, request)
+      .then(dtoFromLibrary);
+  }
+}
--- src/library/library.service.ts
+++ src/library/library.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { LibraryRepository } from './library.repository';
+import { Book, Library, LibraryCreationRequest } from './library.model';
 
 @Injectable()
 export class LibraryService {
@@ -8,7 +9,22 @@ export class LibraryService {
     return this.repo.findByAddress(address);
   }
 
-  findLibraries() {
+  findLibraries(): Promise<Library[]> {
     return this.repo.findAll();
+  }
+
+  create(requestBody: LibraryCreationRequest, request: Request) {
+    // const principal = <Principal>request.user;
+    // principal.roles.include(ROLES.MANAGER)
+    return this.repo.create(requestBody);
+  }
+
+  async getBooks(id: string): Promise<Book[]> {
+    const library = await this.repo
+      .findAllBooks(id)
+      .then((lib) =>
+        lib.orElseThrow(() => new NotFoundException('Library not found')),
+      );
+    return library.books;
   }
 }
